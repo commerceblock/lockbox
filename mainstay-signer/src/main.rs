14964b8eb977934c99f50992968862393437ed57
Dhananjay Purohit <dhananjaypurohit7@gmail.com>
2024-02-19 15:22:58 +0530

    fix: encode to string for sealed data

--- mainstay-signer/src/main.rs
+++ mainstay-signer/src/main.rs
@@ -100,9 +100,9 @@ fn handle_initialize(state: Arc<GlobalState>, key_type: String, share: String) -
         let (seal_data, label) = sealing::seal_recovered_secret(recovered_secret.clone());
 
         let data = db::SealedData {
-            label: "label".to_string(),
-            nonce: "nonce".to_string(),
-            cipher: "cipher".to_string()
+            label: encode(label),
+            nonce: encode(seal_data.nonce),
+            cipher: encode(seal_data.ciphertext)
         };
 
         if let Err(err) = db::save_seal_data_to_db(data, key_type.clone()) {
